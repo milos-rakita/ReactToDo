7357c38069ee7422247ce07a574e73ab2ba77dbc
milos-rakita <milossrb.rakita@gmail.com>
2016-11-28 13:42:08 +0100

    1

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,12 @@ module.exports = {
     },
     resolve: {
         root: __dirname,
+        modulesDirectories: [
+            'node_modules',
+            './app/components'
+        ],
         alias: {
-            main: 'app/components/main.jsx',
             applicationStyles: 'app/styles/app.scss'
-
         },
         extensions: ['', '.js', '.jsx']
     },
