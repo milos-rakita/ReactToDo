fe0eb5e8785124d4dec959b02df2dce91d90db86
milos-rakita <milossrb.rakita@gmail.com>
2016-11-28 14:19:45 +0100

    test

--- app/tests/components/todoList.test.jsx
+++ app/tests/components/todoList.test.jsx
@@ -5,9 +5,28 @@ var TestUtils = require('react-addons-test-utils');
 var $ = require('jQuery');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
 
 describe('TodoList',()=>{
     it('should exist',()=>{
         expect(TodoList).toExist();
     });
+
+    it('should rander on todo component for each tofo item',()=>{
+        var todos = [
+            {
+                id:1,
+                text:'Do something'
+            },
+            {
+                id:2,
+                text:'check mail'
+            }];
+            var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+            var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList,Todo);
+
+            expect(todosComponents.length).toBe(todos.length);
+    });
 });
+
+
