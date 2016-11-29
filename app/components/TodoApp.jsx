be8b73846286d5beda1272eea95f277f5b811015
milos-rakita <milossrb.rakita@gmail.com>
2016-11-29 10:46:13 +0100

    test

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,32 +2,41 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
     getInitialState: function(){
         return{
             todos:[
                 {
-                    id:1,
+                    id:uuid(),
                     text:'Walk the dog'
                 },
                 {
-                    id:2,
+                    id:uuid(),
                     text:'Clean the yard'
                 },
                 {
-                    id:3,
+                    id:uuid(),
                     text:'Leav mail on porch'
                 },
                 {
-                    id:4,
+                    id:uuid(),
                     text:'Play video games'
                 },
             ]
         };
     },
     handleAddTodo: function(text){
-        alert('new todo: '+text)
+        this.setState({
+            todos:[
+                ...this.state.todos,
+                {
+                    id: uuid(),
+                    text: text
+                }
+            ]
+        })
     },
     handleSearch: function(showComplated,searchText){
         this.setState({
---
+++ playground/spread.js
@@ -0,0 +1,33 @@
+// function add(a, b) {
+//     return a + b;
+// }
+
+// console.log(add(3, 1));
+
+// var toAdd = [9, 5];
+// console.log(add(...toAdd));
+
+// var groupA = ['Jen', 'Cory'];
+// var groupB = ['Vikram'];
+// var final = [...groupB, 3, ...groupA];
+
+// console.log(final);
+
+// var person = ['andrew', 25];
+// var personTwo = ['Jen', 29];
+// //hi andrew , you are 25
+// function greet(name, age) {
+//     console.log('Hi ' + name + ', you are ' + age);
+// }
+// greet(...person);
+// greet(...personTwo);
+// console.log('Hi ' + person[0] + ' you are ' + person[1]);
+// console.log('Hi ' + personTwo[0] + ' you are ' + personTwo[1]);
+
+
+// var names = ['Mike', 'Ben'];
+// var final = ['Andrew', ...name];
+// //hi andrew
+// final.forEach(function(name) {
+//     console.log('Hi ' + name);
+// });
